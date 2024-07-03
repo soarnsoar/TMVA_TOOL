254227f750899fa1c57d9738e62e3a1b8fc115f3
Junho <soarnsoar@gmail.com>
2024-07-03 17:02:18 +0900

    add conf

---
+++ config/v2405.4/variables_rank.py
@@ -0,0 +1,5 @@
+bmuon_var=["bmuon_normchi2","bmuon_ntracklayers","bmuon_ntrackhits","bmuon_nmatchedstations","bmuon_nvalidmuonhits","bmuon_nsip3d","bmuon_reltrkiso","bmuon_reliso","bmuon_aeta","bmuon_pt","bmuon_dR_l_j","bmuon_palongjet","bmuon_palongjetratio","bmuon_ptwrtbjet","bmuon_P_jetrest","bjet_charge*bmuon_charge"]
+
+belectron_var=["belectron_full5x5sigmaietaieta","fabs(belectron_detaseed)","belectron_HoverE","belectron_InvEminusInvP","belectron_nsip3d","belectron_reltrkiso","belectron_reliso","belectron_aeta","belectron_pt","belectron_dR_l_j","belectron_palongjet","belectron_palongjetratio","belectron_ptwrtbjet","belectron_P_jetrest","bjet_charge*belectron_charge"]
+
+bjet_var=["bjet_ChargedHadronEnergyFraction","bjet_NeutralHadronEnergyFraction","bjet_NeutralEmEnergyFraction","bjet_ChargedEmEnergyFraction","bjet_MuonEnergyFraction","bjet_ChargedMultiplicity","bjet_NeutralMultiplicity","bjet_pt","bjet_aeta","fabs(bjet_charge)"]
