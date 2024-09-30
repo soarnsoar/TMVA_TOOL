41c185b10c85aa3f5d9dac21ce147ed0c063fb17
Junho <soarnsoar@gmail.com>
2024-09-30 23:35:06 +0900

    submit_run_single_replica.py for v2409.2

--- script/submit_run_single_replica.py
+++ script/submit_run_single_replica.py
@@ -5,15 +5,39 @@ from ExportShellCondorSetup_tamsa import Export
 maindir=os.getenv("JH_TMVA_TOOL_MAINDIR")
 curdir=os.getcwd()
 
-##---v2405.4.3
-version="2405.4.3"
+##---v2409.2
+version="2409.2"
 years=["2016preVFP","2016postVFP","2017","2018"]
 analyzer="EEMu_MuMuE_Method"
 channels={
-    "muon":[3,48,1100,0.1,"G"],
-    "electron":[3,48,600,0.1,"G"],
-    "jet":[5,64,1000,0.2,"G"],
+
+    "2016preVFP":{
+        "muon":[5, 64, 100, 0.2, 'U'],
+        "electron":[5, 256, 1000, 0.4, 'G'],
+        "jet":[5, 64, 1000, 0.2, 'N'],
+    },
+
+    "2016postVFP":{
+        "muon":[5, 128, 1000, 0.4, 'U'],
+        "electron":[5, 128, 1000, 0.4, 'G'],
+        "jet":[10, 256, 1000, 0.2, 'N'],
+    },
+
+    "2017":{
+        "muon":[10, 64, 1000, 0.2, 'G'],
+        "electron":[5, 64, 1000, 0.2, 'G'],
+        "jet":[10, 128, 100, 0.2, 'N'],
+    },
+
+    "2018":{
+        "muon":[10, 256, 500, 0.4, 'G'],
+        "electron":[20, 256, 100, 0.2, 'G'],
+        "jet":[5, 256, 1000, 0.2, 'N'],
+    },
+
+
 }
+
 nepoch=300
 
 submit=1
@@ -23,7 +47,7 @@ iend=istart+Ntotal
 for year in years:
     for channel in channels:
         for i in range(istart,iend):
-            this_params=channels[channel]
+            this_params=channels[year][channel]
             nlayer=this_params[0]
             nnode=this_params[1]
             batchsize=this_params[2]
