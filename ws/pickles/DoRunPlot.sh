261d90484acc2e6194f48da78c198b1c03ac416c
Junho <soarnsoar@gmail.com>
2025-12-19 19:05:05 +0900

    pkl files and script for optimizaztion

---
+++ ws/pickles/DoRunPlot.sh
@@ -0,0 +1,10 @@
+#jet2016postVFP/run.done  jet2016preVFP/run.done  jet2017/run.done  jet2018/run.done  muon2016postVFP/run.done  muon2016preVFP/run.done
+#ARR_OBJ=(muon electron jet)
+mkdir -p logs/
+ARR_OBJ=(muon electron jet)
+ARR_YEAR=(2016preVFP 2016postVFP 2017 2018)
+for OBJ in ${ARR_OBJ[@]};do
+    for YEAR in ${ARR_YEAR[@]};do
+	python3 runPlot.py ${OBJ} ${YEAR} &> logs/${OBJ}__${YEAR}.log&
+    done
+done
---
+++ ws/pickles/DoRunPlot_TEMP.sh
@@ -0,0 +1,17 @@
+#jet2016postVFP/run.done  jet2016preVFP/run.done  jet2017/run.done  jet2018/run.done  muon2016postVFP/run.done  muon2016preVFP/run.done
+#ARR_OBJ=(muon electron jet)
+mkdir -p logs/
+
+##
+OBJ=jet
+YEAR=2016preVFP
+python3 runPlot.py ${OBJ} ${YEAR} &> logs/${OBJ}__${YEAR}.log&
+##
+OBJ=jet
+YEAR=2017
+python3 runPlot.py ${OBJ} ${YEAR} &> logs/${OBJ}__${YEAR}.log&
+
+OBJ=jet
+YEAR=2018
+python3 runPlot.py ${OBJ} ${YEAR} &> logs/${OBJ}__${YEAR}.log&
+
