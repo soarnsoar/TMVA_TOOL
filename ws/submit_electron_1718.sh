620b4032d43105f7bd7bb01d83a03f3ed9345400
jhchoi <soarnsoar@gmail.com>
2026-08-17 20:47:41 +0900

    submit_electron_1718

---
+++ ws/submit_electron_1718.sh
@@ -0,0 +1,12 @@
+mkdir -p logs
+ARR_SCRIPT=(
+    #submit_run_single_SCAN_BDT_ELECTRON_16a.py  submit_run_single_SCAN_BDT_ELECTRON_16b.py
+    submit_run_single_SCAN_BDT_ELECTRON_17.py  submit_run_single_SCAN_BDT_ELECTRON_18.py
+
+
+)
+
+for script in ${ARR_SCRIPT[@]};do
+    ${script} &> logs/${script}.electrononlysub.log &
+    sleep 30
+done
