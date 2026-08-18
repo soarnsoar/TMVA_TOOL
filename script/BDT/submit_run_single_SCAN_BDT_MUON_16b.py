7df318d5f90049d2e9d57ff0c286c5cb4b8b1709
jhchoi <soarnsoar@gmail.com>
2026-08-18 19:32:52 +0900

    submit_run_single_SCAN_BDT_MUON_16b.py wider range

--- script/BDT/submit_run_single_SCAN_BDT_MUON_16b.py
+++ script/BDT/submit_run_single_SCAN_BDT_MUON_16b.py
@@ -65,12 +65,12 @@ list_nCuts=['30','40','50']
 list_SeparationType=["CrossEntropy"]
 
 ##--step3
-list_BaggedSampleFraction=['0.4', '0.5', '0.6']
+list_BaggedSampleFraction=['0.1','0.2','0.4', '0.5', '0.6']
 list_MaxDepth=['4','6','8','10']
 list_MinNodeSize=['0.1','0.5', '1', '2.5']
 list_NTrees=['200','500','800']
 list_Shrinkage=['0.01', '0.005', '0.001']
-list_nCuts=['30','40','50']
+list_nCuts=['30','40','50','80','100']
 
 
 submit=1
