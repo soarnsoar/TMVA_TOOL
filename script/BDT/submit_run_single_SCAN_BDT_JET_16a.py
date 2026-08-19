93af286b538eb40eb0ec32544c014d0a31d044b1
jhchoi <soarnsoar@gmail.com>
2026-08-19 16:18:49 +0900

    step3 add gini jet

--- script/BDT/submit_run_single_SCAN_BDT_JET_16a.py
+++ script/BDT/submit_run_single_SCAN_BDT_JET_16a.py
@@ -55,21 +55,23 @@ version="2608.2"
 
 list_BoostType=["Grad"]
 transforms=["I","G"]
-list_BaggedSampleFraction=['0.6', '0.8']
-list_MaxDepth=['4','6','8']
-list_MinNodeSize=['0.5', '1', '2.5']
-list_NTrees=['1000','1500','3000']
-list_Shrinkage=['0.01', '0.005', '0.001']
-list_nCuts=['30','40','50']
+
+#list_BaggedSampleFraction=['0.6', '0.8']
+#list_MaxDepth=['4','6','8']
+#list_MinNodeSize=['0.5', '1', '2.5']
+#list_NTrees=['1000','1500','3000']
+#list_Shrinkage=['0.01', '0.005', '0.001']
+#list_nCuts=['30','40','50']
+
 list_SeparationType=["CrossEntropy","GiniIndex"]
 
 ##--step3
-#list_BaggedSampleFraction=['0.6', '0.8']
-#list_MaxDepth=['4','6','8']
-#list_MinNodeSize=['0.1','0.5', '1', '2.5']
-#list_NTrees=['1500','3000','5000']
-#list_Shrinkage=['0.01', '0.005', '0.001']
-#list_nCuts=['40','50','80','100']
+list_BaggedSampleFraction=['0.6', '0.8']
+list_MaxDepth=['4','6','8']
+list_MinNodeSize=['0.1','0.5', '1', '2.5']
+list_NTrees=['1500','3000','5000']
+list_Shrinkage=['0.01', '0.005', '0.001']
+list_nCuts=['40','50','80','100']
 
 
 submit=1
--- script/BDT/submit_run_single_SCAN_BDT_JET_16b.py
+++ script/BDT/submit_run_single_SCAN_BDT_JET_16b.py
@@ -55,21 +55,23 @@ version="2608.2"
 
 list_BoostType=["Grad"]
 transforms=["I","G"]
-list_BaggedSampleFraction=['0.2', '0.6']
-list_MaxDepth=['4','6','8']
-list_MinNodeSize=['0.5', '1', '2.5']
-list_NTrees=['200','500','800']
-list_Shrinkage=['0.01', '0.005', '0.001']
-list_nCuts=['5','30','40','50']
+
+#list_BaggedSampleFraction=['0.2', '0.6']
+#list_MaxDepth=['4','6','8']
+#list_MinNodeSize=['0.5', '1', '2.5']
+#list_NTrees=['200','500','800']
+#list_Shrinkage=['0.01', '0.005', '0.001']
+#list_nCuts=['5','30','40','50']
+
 list_SeparationType=["CrossEntropy","GiniIndex"]
 
 ##--step3
-#list_BaggedSampleFraction=['0.1','0.2', '0.4', '0.5']
-#list_MaxDepth=['4','6','8']
-#list_MinNodeSize=['0.1','0.5', '1', '2.5']
-#list_NTrees=['200','500','800']
-#list_Shrinkage=['0.01', '0.005', '0.001']
-#list_nCuts=['40','50','80','100']
+list_BaggedSampleFraction=['0.1','0.2', '0.4', '0.5']
+list_MaxDepth=['4','6','8']
+list_MinNodeSize=['0.1','0.5', '1', '2.5']
+list_NTrees=['200','500','800']
+list_Shrinkage=['0.01', '0.005', '0.001']
+list_nCuts=['40','50','80','100']
 
 
 submit=1
